7e0ccaa9d75bc6ec824b0750379258da64fcb22a
gaspaw <gaspard.calligaro@edu.devinci.fr>
2025-04-01 17:06:39 +0200

    Update Program.cs
    encore des modifs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World, c'est moi lbjkm!");
+            Console.WriteLine("Hello World, c'est moi lbjkm! je modifie en bien");
         }
     }
 }
